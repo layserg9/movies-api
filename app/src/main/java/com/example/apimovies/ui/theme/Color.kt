d696aaca0263860e78d42f744f260f24ef9ca17a
v.mirolyubov <v.mirolyubov@funflow.ru>
2025-03-27 09:39:04 +0300

    added new colors

--- app/src/main/java/com/example/apimovies/ui/theme/Color.kt
+++ app/src/main/java/com/example/apimovies/ui/theme/Color.kt
@@ -9,3 +9,131 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val TintsRedLight = Color(0xFFFA4737)
+val TintsOrangeLight = Color(0xFFFB8D0D)
+val TintsYellowLight = Color(0xFFFFC20B)
+val TintsGreenLight = Color(0xFF26BB6A)
+val TintsBrandLight = Color(0xFF00B8B8)
+val TintsBlueLight = Color(0xFF00B2FF)
+val TintsDeepBlueLight = Color(0xFF127FFF)
+val TintsPurpleLight = Color(0xFFB225D6)
+val TintsPinkLight = Color(0xFFF2226B)
+val TintsBrownLight = Color(0xFFBE6548)
+val TintsSilverLight = Color(0xFFA9BACF)
+
+val TintsRedDark = Color(0xFFFA4D47)
+val TintsOrangeDark = Color(0xFFFC9A08)
+val TintsYellowDark = Color(0xFFFFCB11)
+val TintsGreenDark = Color(0xFF24C16C)
+val TintsBrandDark = Color(0xFF00B2A7)
+val TintsBlueDark = Color(0xFF1BB9FB)
+val TintsDeepBlueDark = Color(0xFF1E8BFA)
+val TintsPurpleDark = Color(0xFFBC1CE3)
+val TintsPinkDark = Color(0xFFF52C73)
+val TintsBrownDark = Color(0xFFCD6B4B)
+val TintsSilverDark = Color(0xFFC7D6E7)
+
+// Background colors
+
+val BackgroundPrimaryLight = Color(0xFFFFFFFF)
+val BackgroundSecondaryLight = Color(0xFFF9F9F9)
+val BackgroundTertiaryLight = Color(0xFFFFFFFF)
+val BackgroundModalPrimaryLight = Color(0xFFFFFFFF)
+val BackgroundModalSecondaryLight = Color(0xFFF9F9F9)
+val BackgroundModalTertiaryLight = Color(0xFFFFFFFF)
+
+val BackgroundPrimaryDark = Color(0xFF000000)
+val BackgroundSecondaryDark = Color(0xFF1C1C1E)
+val BackgroundTertiaryDark = Color(0xFF2C2C2E)
+val BackgroundModalPrimaryDark = Color(0xFF1C1C1E)
+val BackgroundModalSecondaryDark = Color(0xFF2C2C2E)
+val BackgroundModalTertiaryDark = Color(0xFF3A3A3C)
+
+val Elevation00DpLight = Color(0xFFFFFFFF)
+val Elevation01DpLight = Color(0xFFFFFFFF)
+val Elevation02DpLight = Color(0xFFFFFFFF)
+val Elevation03DpLight = Color(0xFFFFFFFF)
+val Elevation04DpLight = Color(0xFFFFFFFF)
+val Elevation06DpLight = Color(0xFFFFFFFF)
+val Elevation08DpLight = Color(0xFFFFFFFF)
+val Elevation12DpLight = Color(0xFFFFFFFF)
+val Elevation16DpLight = Color(0xFFFFFFFF)
+val Elevation24DpLight = Color(0xFFFFFFFF)
+
+val Elevation00DpDark = Color(0xFF121212)
+val Elevation01DpDark = Color(0xFF1E1E1E)
+val Elevation02DpDark = Color(0xFF232323)
+val Elevation03DpDark = Color(0xFF252525)
+val Elevation04DpDark = Color(0xFF272727)
+val Elevation06DpDark = Color(0xFF2C2C2C)
+val Elevation08DpDark = Color(0xFF2E2E2E)
+val Elevation12DpDark = Color(0xFF333333)
+val Elevation16DpDark = Color(0xFF363636)
+val Elevation24DpDark = Color(0xFF383838)
+
+val FillsPrimaryLight = Color(0x38787880)
+val FillsSecondaryLight = Color(0x29787880)
+val FillsTertiaryLight = Color(0x1F767680)
+val FillsQuaternaryLight = Color(0x1A747480)
+val FillsQuaternaryLightLight = Color(0x12747480)
+
+val FillsPrimaryDark = Color(0x5C787880)
+val FillsSecondaryDark = Color(0x52787880)
+val FillsTertiaryDark = Color(0x3D767680)
+val FillsQuaternaryDark = Color(0x2E767680)
+val FillsQuaternaryLightDark = Color(0x33767680)
+
+// Labels colors
+
+val LabelsPrimaryLight = Color(0xFF000000)
+val OnSurfaceHighLight = Color(0xDE000000)
+val LabelsSecondaryLight = Color(0x993C3C43)
+val LabelsTertiaryLight = Color(0x523C3C43)
+val LabelsQuaternaryLight = Color(0x333C3C43)
+
+val LabelsPrimaryDark = Color(0xFFFFFFFF)
+val OnSurfaceHighDark = Color(0xDEFFFFFF)
+val LabelsSecondaryDark = Color(0x99EBEBF5)
+val LabelsTertiaryDark = Color(0x52EBEBF5)
+val LabelsQuaternaryDark = Color(0x33EBEBF5)
+
+// On primary colors
+
+val OnPrimaryPrimaryLight = Color(0xFFFFFFFF)
+val OnPrimaryHighLight = Color(0xDEFFFFFF)
+val OnPrimaryMediumLight = Color(0x99FFFFFF)
+val OnPrimaryDisabledLight = Color(0x61FFFFFF)
+val OnPrimaryLightLight = Color(0x3DFFFFFF)
+
+val OnPrimaryPrimaryDark = Color(0xFF000000)
+val OnPrimaryHighDark = Color(0xDE000000)
+val OnPrimaryMediumDark = Color(0x99000000)
+val OnPrimaryDisabledDark = Color(0x61000000)
+val OnPrimaryLightDark = Color(0x3D000000)
+
+//Separator colors
+
+val SeparatorOpaqueLight = Color(0xFFC6C6C8)
+val SeparatorNonOpaqueLight = Color(0x293C3C43)
+val SeparatorElementsLight = Color(0x0F000000)
+
+val SeparatorOpaqueDark = Color(0xFF38383A)
+val SeparatorNonOpaqueDark = Color(0x8A545458)
+val SeparatorElementsDark = Color(0x47000000)
+
+// Static colors
+
+val StaticPrimaryDark = Color(0xFF000000)
+val StaticSecondaryDark = Color(0xBF000000)
+val StaticTertiaryDark = Color(0x99272728)
+val StaticQuaternaryDark = Color(0x6638383A)
+val StaticQuaternaryLightDark = Color(0x33000000)
+val BaseLight12PcBlack = Color(0x1F000000)
+val BaseLight6PcBlack = Color(0x0F000000)
+
+val StaticPrimaryLight = Color(0xFFFFFFFF)
+val StaticSecondaryLight = Color(0xBFFFFFFF)
+val StaticTertiaryLight = Color(0x99FFFFFF)
+val StaticQuaternaryLight = Color(0x66FFFFFF)
+val StaticQuaternaryLightLight = Color(0x26FFFFFF)
