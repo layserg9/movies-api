3820b2910d90d285b918786a839a98deebfbeffc
v.mirolyubov <v.mirolyubov@funflow.ru>
2025-04-29 14:43:05 +0300

    added empty favorites list disclaimer

--- app/src/main/java/com/example/apimovies/presentation/compose/FavoritesListScreen.kt
+++ app/src/main/java/com/example/apimovies/presentation/compose/FavoritesListScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -52,6 +53,18 @@ fun FavoritesListScreen(
         contentPadding = PaddingValues(horizontal = 10.dp)
 
     ) {
+        if (list.isEmpty()) {
+            item {
+                Text(
+                    modifier = Modifier
+                        .padding(top = 25.dp)
+                        .fillMaxWidth(),
+                    text = "Список избранного пуст",
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.titleMedium,
+                )
+            }
+        }
         items(items = list, key = { it.id }) { movieItem ->
             MovieItem(
                 movie = movieItem,
@@ -105,7 +118,7 @@ private fun MovieInfo(
     movie: Movie
 ) {
     val text = listOf(movie.alternativeName, movie.year.takeIf { it != 0L }.toString())
-        .filter { it.isNotBlank()}
+        .filter { it.isNotBlank() }
         .joinToString(", ")
 
     Column(
